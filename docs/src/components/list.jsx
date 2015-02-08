6711738c3923c8c8cd42d553fc0fe9244aa2f670
Brent Jackson <jacksonblack@gmail.com>
2015-02-08 16:17:35 -0500

    Remove color button

--- docs/src/components/list.jsx
+++ docs/src/components/list.jsx
@@ -19,15 +19,31 @@ module.exports = React.createClass({
     this.props.handleChange(this.state.colors);
   },
 
+  removeColor: function(e) {
+    var key = e.target.dataset.key;
+    var colors = this.state.colors;
+    console.log('remove', key);
+    delete colors[key];
+    this.setState({ colors: colors });
+    this.props.handleChange(this.state.colors);
+  },
+
   renderItem: function(key) {
     var color = this.state.colors[key];
     return (
-      <li>
+      <li className="flex flex-stretch">
         <input type="text"
           id={key}
-          className="full-width field-dark"
+          className="full-width m0 field-dark"
           value={color}
           onChange={this.updateColor} />
+        <a href="#!"
+          data-key={key}
+          onClick={this.removeColor}
+          className="h3 button button-narrow button-nav-dark"
+          title="Remove color">
+            &times;
+        </a>
       </li>
     )
   },
