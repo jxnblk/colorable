9104dc09f360ee5f00d9f8721fd134f53257d84e
Brent Jackson <jacksonblack@gmail.com>
2015-02-19 12:45:00 -0500

    Basic form

--- docs/src/components/text-app.jsx
+++ docs/src/components/text-app.jsx
@@ -4,6 +4,9 @@ var React = require('react');
 var qs = require('query-string');
 var colorable = require('../../..');
 
+var ColorPreview = require('./color-preview.jsx');
+var HslForm = require('./hsl-form.jsx');
+
 module.exports = React.createClass({
 
   getInitialState: function() {
@@ -29,21 +32,55 @@ module.exports = React.createClass({
   },
 
   render: function() {
+    var self = this;
     var foreground = this.state.foreground;
     var background = this.state.background;
+    try {
+      var color = colorable([foreground, background])[0];
+      color.combo = color.combinations[0];
+    } catch(e) {
+      var color = {
+        combo: {
+          contrast: 0
+        }
+      };
+    }
+
+    var handleForegroundChange = function(e) {
+      self.setForeground(e.target.value);
+    };
+    var handleBackgroundChange = function(e) {
+      self.setBackground(e.target.value);
+    };
     var style = {
       color: foreground,
       backgroundColor: background,
       minHeight: '100vh',
     };
-    var color = colorable([foreground, background])[0];
-    var combo = color.combinations[0];
+    var innerStyle = {
+      maxWidth: '48rem',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    };
+
+    console.log('render');
+
     return (
       <div className="px2" style={style}>
-        <div className="py3">
+        <div className="py3" style={innerStyle}>
+          <ColorPreview {...color} />
           <h1>TextApp</h1>
-          <div>{color.hex} on {combo.hex}</div>
-          <div>{combo.contrast}</div>
+          <div>{color.hex} on {color.combo.hex}</div>
+          <div>{color.combo.contrast}</div>
+          <form className="sm-flex flex-center mxn2">
+            <div className="sm-col-6 px2">
+              <label className="h5 bold">Foreground</label>
+              <input type="text"
+                value={foreground}
+                onChange={handleForegroundChange}
+                className="block full-width field-dark" />
+            </div>
+          </form>
         </div>
       </div>
     )
