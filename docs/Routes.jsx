f5c6f1f3bf1cfd3bb686db1bd1238168dfc081cc
jxnblk <jacksonblack@gmail.com>
2015-06-07 13:40:36 -0400

    Adjust routes

--- docs/Routes.jsx
+++ docs/Routes.jsx
@@ -17,13 +17,13 @@ var Routes = (
     <Route path='demos/text' handler={TextDemo} />
     <Route path='demos/matrix' handler={MatrixDemo} />
     <Route path='/colorable/' name='home' handler={Index}>
-      <Route path='demos/' name='demos' handler={Demos} />
-      <Route path='demos/text/' name='text' handler={TextDemo} />
-      <Route path='demos/matrix/' name='matrix' handler={MatrixDemo} />
+      <Route path='demos' name='demos' handler={Demos} />
+      <Route path='demos/text' name='text' handler={TextDemo} />
+      <Route path='demos/matrix' name='matrix' handler={MatrixDemo} />
     </Route>
-    <Redirect from='/colorable/demos/' to='demos' />
-    <Redirect from='/colorable/demos/text/' to='text' />
-    <Redirect from='/colorable/demos/matrix/' to='matrix' />
+    <Redirect from='/colorable/demos/' to='/demos' />
+    <Redirect from='/colorable/demos/text/' to='/demos/text' />
+    <Redirect from='/colorable/demos/matrix/' to='/demos/matrix' />
   </Route>
 )
 
