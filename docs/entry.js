5b798ca10f30a21e7333b88084c6b2d09d0a8803
jxnblk <jacksonblack@gmail.com>
2015-06-07 14:14:12 -0400

    Move ga script

--- docs/entry.js
+++ docs/entry.js
@@ -13,6 +13,13 @@ if (typeof document !== 'undefined') {
   // Tweet button
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "twitter-wjs");
 
+  // GA
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-4603832-6', 'auto');
+  ga('send', 'pageview');
 }
 
 module.exports = function render(locals, callback) {
