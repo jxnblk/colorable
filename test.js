b49a6d265fd1cc06c13d449b85841ac35f31d44b
John Otander <johnotander@gmail.com>
2016-03-24 10:37:24 -0600

    Add contrast testing

--- test.js
+++ test.js
@@ -3,14 +3,22 @@ import isPresent from 'is-present'
 import colorable from './'
 
 test('it returns an array of color combinations', t => {
-  t.plan(1)
+  t.plan(4)
 
   const colors = {
-    red: 'red',
-    green: 'green',
-    blue: 'blue'
+    red: 'tomato',
+    blue: 'blue',
+    gray1: '#333',
+    gray2: '#444'
   }
 
   const colorCombos = colorable(colors)
+  const gray1Combos = colorCombos[2].combinations
+  const redContrast = gray1Combos[0].contrast
+  const gray2Contrast = gray1Combos[2].contrast
+
   t.true(isPresent(colorCombos))
+  t.same(colorCombos.length, 4)
+  t.true(redContrast > 4)
+  t.true(gray2Contrast < 2)
 })
